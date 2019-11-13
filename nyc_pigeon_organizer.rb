a426e278b961ec8e96953aa4c19f74d38c3c7d66
Andrew Richards <arichards4814@gmail.com>
2019-11-13 13:13:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+newHash = {}
+
+data.each do |key, value|
+    value.each do |key2, value2|
+      value2.map do |ele|
+        
+        if newHash[ele] == nil
+          #create empty k v pair for name
+          newHash[ele] = {}
+        end
+
+        if newHash[ele][key] == nil
+          #create empty k v pair for attribute
+          newHash[ele][key] = []
+        end
+          
+          #push the new values into our new array
+        newHash[ele][key].push(key2.to_s)
+
+      end
+      
+    end
+    
+end
+
+newHash
 end

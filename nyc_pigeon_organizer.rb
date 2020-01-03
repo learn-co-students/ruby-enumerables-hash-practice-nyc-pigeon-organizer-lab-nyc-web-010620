4efbb134192ea4d1520c7a3668549f60175c7927
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 15:31:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = new.Hash()
+  
+  data.each do |pigeon, value|
+    value.each do |details, names|
+      names.each{ |name|
+        if !(result.has_key?(name))
+          result[name] = {}
+        end
+        
+        if !(result[name].has_key?(details))
+          result[name][details] = {}
+        end
+        
+        if !(result[name][pigeon].include?(details))
+          result[name][pigeon].push(details)
+        end
+      }
+     end
+    end
+  end
+  return result
 end
